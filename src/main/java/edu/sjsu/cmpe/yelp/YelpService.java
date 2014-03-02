363e0fecfa556572e9fe3aa36b597c883aaeefec
savanibharat <savanibharat@gmail.com>
2014-03-01 17:35:10 -0800

    retrived a address of business

--- src/main/java/edu/sjsu/cmpe/yelp/YelpService.java
+++ src/main/java/edu/sjsu/cmpe/yelp/YelpService.java
@@ -1,5 +1,47 @@
 package edu.sjsu.cmpe.yelp;
 
+import java.net.UnknownHostException;
+import java.util.Random;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+import org.codehaus.jettison.json.JSONTokener;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+/**
+ * The Class FindTest.
+ */
+/**
+ * @author Savani Bharat
+ *
+ */
 public class YelpService {
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 * @throws UnknownHostException the unknown host exception
+	 * @throws JSONException 
+	 */
+	public static void main(String[] args) throws UnknownHostException, JSONException {
+
+		MongoClient client = new MongoClient();
+		DB db = client.getDB("YelpNewDataSet");
+		DBCollection collection = db.getCollection("business");
+
+		System.out.println("findOne()");
+		BasicDBObject query=new BasicDBObject();
+		BasicDBObject fields=new BasicDBObject("full_address",1).append("_id", false);
+		DBObject doc = collection.findOne(query,fields);
+		//System.out.println(doc.get("full_address"));
+		String s=doc.get("full_address").toString();
+		System.out.println(s);
+	}
 }
